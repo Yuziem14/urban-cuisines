5ebc69d9a242ca8809a02508a450910928cd0219
yuziem14 <yuri.fnunes@gmail.com>
2020-09-06 12:42:04 -0300

    chore: create custom leaflet marker icons

---
+++ web/src/components/Map/icons.ts
@@ -0,0 +1,11 @@
+import { Icon } from 'leaflet';
+
+export const MapPinIcon = new Icon({
+  iconUrl: '/icons/map-pin.svg',
+  iconSize: [24, 24],
+});
+
+export const RestaurantIcon = new Icon({
+  iconUrl: '/icons/restaurant-marker.svg',
+  iconSize: [24, 24],
+});
